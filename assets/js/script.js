2ed23e700101d5c5c796d0425821037f31db46b9
Carlos Poblete Araya <capoblete@creasys.cl>
2025-07-15 21:07:57 -0400

    creacion de objeto cliente y arreglo

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+// Creacion del objeto clienteObjeto
+let clienteObjeto = {
+    id: 0,
+    nombre: "",
+    apellido: "",
+    email: "",
+    telefono: "",
+    activo: true
+}
+
+//Creacion del arreglo clientes que va a almacenar cada uno de los clienteObjeto
+let clientes = []
